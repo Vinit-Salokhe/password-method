5d3fb6dc8c6d11ee6053524c955b7d793c5d4637
Vinit-Salokhe <vinitsalokhe3540@gmail.com>
2021-05-29 15:49:11 +0530

    my first commit

---
+++ NewClass.java
@@ -0,0 +1,49 @@
+package com.pro.text;
+
+import java.util.Random;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		int length = 8;
+		int Strength = 2;
+		System.out.println(Password(length, Strength));
+	}
+
+	static char[] Password(int len, int Strength) {
+		System.out.println("Generating password : ");
+		System.out.print("Your password is : ");
+
+		String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String Small_chars = "abcdefghijklmnopqrstuvwxyz";
+		String numbers = "0123456789";
+		String symbols = "!@#$%^&*_=+-/.?<>)";
+
+		String values = Capital_chars + Small_chars;
+		String values1 = Capital_chars + Small_chars + numbers;
+		String values2 = Capital_chars + Small_chars + numbers + symbols;
+
+		Random rn = new Random();
+
+		char[] password = new char[len];
+		if (Strength == 1) {
+			for (int i = 0; i < len; i++) {
+				password[i] = values.charAt(rn.nextInt(values.length()));
+			}
+			return password;
+
+		} else if (Strength == 2) {
+			for (int i = 0; i < len; i++) {
+				password[i] = values1.charAt(rn.nextInt(values1.length()));
+			}
+			return password;
+		} else if (Strength == 3) {
+			for (int i = 0; i < len; i++) {
+				password[i] = values2.charAt(rn.nextInt(values2.length()));
+			}
+			return password;
+		}
+		return password;
+	}
+
+}
